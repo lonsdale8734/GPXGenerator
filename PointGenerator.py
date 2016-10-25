6a0cedebb265ea02df9f0c9fdd69f726b96ce4c4
lons <lonsdale8734@gmail.com>
2016-10-26 00:32:41 +0800

    add PointGenerator -- not done

---
+++ PointGenerator.py
@@ -0,0 +1,26 @@
+
+class PointGenerator(object):
+	"""docstring for PointGenerator"""
+
+	__breakPoints = []
+	__breakPointIndex = 0
+
+	__speed = 1# 1 - 10
+	__toBeBack = False
+	__toWalkAround = False
+
+	__location = ()
+
+	
+	def __init__(self, arg):
+		super(PointGenerator, self).__init__()
+		self.arg = arg
+
+	def setBreakPoints():
+		pass
+
+	def setSpeed():
+		pass
+
+	def getNextPoint(direction=None):
+		pass
